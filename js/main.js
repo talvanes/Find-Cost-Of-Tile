622ddc306e2d9db372232e4685808b68362a5cc4
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2015-05-03 14:42:54 -0300

    Javascript code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,19 @@ $(document).ready(function(){
 		to calculate the result
 		*/
 	$("#costPerTileForm").on("input", function(){
+		// Get Width and Height
+		var width = parseFloat($('#width').val());
+		var height = parseFloat($('#height').val());
+		// Get cost per square meter of tile
+		var costPerSqMeter = parseFloat($('#cost').val());
+
+		// Let's perform calculation
+		var totalCost = costPerSqMeter * (width * height);
+		if(!isNaN(totalCost)){
+			$('#total').val('$ ' + totalCost.toFixed(2));
+		} else {
+			$('#total').val('$ 0.00');
+		}
 
 	});
 });
